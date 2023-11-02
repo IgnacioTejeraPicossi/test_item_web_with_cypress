3c6fcf5f1233da70e8454fa394fc4886e3b1ae6a
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2023-11-02 14:59:51 +0100

    error_item_dekker_din_master

---
+++ cypress/e2e/Item test/error_item_dekker_din_master.cy.js
@@ -0,0 +1,22 @@
+it('Backend utvikler click',()=>{
+  cy.visit('/')
+  cy.contains('Velkommen til Item');
+
+  //Jobbe hos oss
+  cy.get(':nth-child(1) > [href="/karriere"]').click();
+  cy.contains('Item dekker din master');
+
+
+  //Kompetanseutvikling
+  cy.get('.block > .list-unstyled > :nth-child(4) > a').click();
+  cy.contains('Kompetanseutvikling');
+  cy.contains('Item dekker din master');
+
+
+  //Item dekker din master
+
+  cy.get(':nth-child(3) > .linker-arrow').click();
+  cy.contains('Noe har gått galt'); //error
+  //cy.contains('Item dekker din master')
+  
+} )
